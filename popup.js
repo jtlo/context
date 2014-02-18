563edcf653eecec71611109715f1e1f191f2bd95
jtlo <jtlo.jtlo.jtlo@gmail.com>
2014-02-18 23:34:26 +0900

    Implement several features
    Implement recent pages widget, implement to remember ordering of widgets

--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
+var delta_order = 100.0;
+var debug_context = {};
 
 var context_widgets = {
 
@@ -28,6 +30,21 @@ var context_widgets = {
         return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4() +S4());
     },
 
+    update_order: function (store, uuid, newOrd, callback) {
+        console.log("Update y-index of widget [" + uuid + "] to " + newOrd);
+        var key = "widget_" + uuid;
+        store.get(key, function(o) {
+            var update = {}
+            var widget = o[key];
+            widget.ord = newOrd;
+            update[key] = widget;
+
+            store.set(update, function() {
+                callback();
+            });
+        });
+    },
+
     add_widget: function(store, key, new_widget, callback) {
         store.get('widgets', function(o) {
             var keys = [];
@@ -46,9 +63,79 @@ var context_widgets = {
         });
     },
 
+    get_last_widget_order: function() {
+        var ord = parseFloat($(".widget_body:last").data("ord"));
+        return ord || 0.0;
+    },
+
+
+    add_recent_pages: function() {
+        $("#rp_add").button().click(function (ev) {
+            ev.preventDefault();
+            var uuid = context_widgets.get_uuid();
+            var new_widget = {
+                'ord': context_widgets.get_last_widget_order() + delta_order,
+                'uuid': uuid,
+                'type': 'RP',
+                'condtype': 'url_re',
+                'condkey': $('#rp_cond_field')[0].value,
+                'search': $('#rp_key_field')[0].value,
+                'num': parseInt($('#rp_num_field')[0].value)
+            };
+            key = "widget_" + uuid;
+
+            try {
+                context_widgets.add_widget(chrome.storage.sync, key, new_widget, function() {
+                    context_widgets.show_widgets();
+                });                
+            } catch(err) {
+                console.log(err);
+                throw err;
+            }
+  
+        });
+
+        chrome.tabs.query({active: true, windowId: chrome.windows.WINDOW_ID_CURRENT}, function (tabs) {
+            var tab = tabs[0];
+            var url = tab.url;
+            var title = tab.title;
+            var domain = context_widgets.get_domain(url);
+
+            $('#rp_cond_field')[0].value = domain;
+            $('#rp_key_field')[0].value = domain;
+            
+            $('#rp_num_field').spinner().spinner("value", 3);
+
+            context_widgets.transit_view("#add_recent_pages_form");
+        });
+
+    },
+
     add_link: function() {
-        //chrome.tabs.getCurrent(function (tab) {
-        chrome.tabs.query({"active": true}, function (tabs) {
+        $("#add").button().click(function (ev) {
+            ev.preventDefault();
+            var uuid = context_widgets.get_uuid();
+            var new_widget = {
+                'ord': context_widgets.get_last_widget_order() + delta_order,
+                'uuid': uuid,
+                'type': 'link',
+                'title': $('#title_field')[0].value,
+                'condtype': 'url_re',
+                'condkey': $('#cond_field')[0].value,
+                'link_href': $('#link_field')[0].value
+            };
+            key = "widget_" + uuid;
+            try {
+                context_widgets.add_widget(chrome.storage.sync, key, new_widget, function() {
+                    context_widgets.show_widgets();
+                });                
+            } catch(err) {
+                console.log(err);
+                throw err;
+            }
+        });
+        
+        chrome.tabs.query({active: true, windowId: chrome.windows.WINDOW_ID_CURRENT}, function (tabs) {
             var tab = tabs[0];
             var url = tab.url;
             var title = tab.title;
@@ -57,33 +144,33 @@ var context_widgets = {
             $('#cond_field')[0].value = domain;
             $('#link_field')[0].value = url;
             $('#title_field')[0].value = title;
-            $("#add").button().click(function (ev) {
-                ev.preventDefault();
-                var uuid = context_widgets.get_uuid();
-                var new_widget = {
-                    'uuid': uuid,
-                    'type': 'link',
-                    'title': $('#title_field')[0].value,
-                    'condtype': 'url_re',
-                    'condkey': $('#cond_field')[0].value,
-                    'link_href': $('#link_field')[0].value
-                };
-                key = "widget_" + uuid;
-                try {
-                    context_widgets.add_widget(chrome.storage.sync, key, new_widget, function() {
-                        context_widgets.show_widgets();
-                    });                
-                } catch(err) {
-                    console.log(err);
-                    throw err;
-                }
-            });
             context_widgets.transit_view("#add_link_form");
         });
     },
 
     render_html: function(widget) {
-        return '<span class="ui-icon ui-icon-bookmark" style="float:left; margin-left:10px; margin-right:10px;"></span> <a href="' + widget.link_href + '" class="widget_link">' + widget.title + '</a>'
+        if (widget.type == 'link') {
+            return '<div class="link_widget_body"><span class="ui-icon ui-icon-bookmark" style="float:left; margin-left:10px; margin-right:10px;"></span> <a href="' + widget.link_href + '" class="widget_link">' + widget.title + '</a> </div>';
+        } else if (widget.type == 'RP') {
+            var stt = Date.now() - 1000.0 * 60.0 * 60.0 * 24.0 * 7.0;
+            chrome.history.search(
+                {text: widget.search, maxResults: widget.num, startTime: stt}, 
+                function (items) {
+                    var html = '<span class="rp_header">Recent visits: </span>';
+                    for (var i = 0; i < items.length; ++ i) {
+                        html += '<a class="widget_link" href="'+items[i].url+'"><span style="display: inline-block" class="ui-icon ui-icon-arrowreturnthick-1-w"></span>'+items[i].title+'</a>';
+                        if (i + 1 < items.length) html += ", ";
+                    }
+                    $("#RP_" + widget.uuid).html(html);
+                    $(".widget_link").click(function(ev) {
+                        ev.preventDefault();
+                        console.log(this.href);
+                        context_widgets.open_link(this.href);
+                    });
+                });
+
+            return '<div class="RP_widget_body" id="RP_' + widget.uuid+ '">Show RP Widget here</div>';
+        }
     },
 
     check_condition: function(tab, widget) {
@@ -111,7 +198,7 @@ var context_widgets = {
     },
 
     open_link: function(url) {
-        chrome.tabs.query({"active": true}, function (tabs) {
+        chrome.tabs.query({active: true, windowId: chrome.windows.WINDOW_ID_CURRENT}, function (tabs) {
             var tab = tabs[0];
             chrome.tabs.update(tab.id, {url: url}, function () {
             });
@@ -119,21 +206,38 @@ var context_widgets = {
     },
 
 
-
     show_widgets: function() {
-        chrome.tabs.query({"active": true}, function (tabs) {
+        chrome.tabs.query({active: true, windowId: chrome.windows.WINDOW_ID_CURRENT}, function (tabs) {
             var tab = tabs[0];
             chrome.storage.sync.get('widgets', function(o1) {
                 var keys = []
                 if ('widgets' in o1) keys = o1['widgets'];
                 chrome.storage.sync.get(keys, function (o2) {
                     var html = '';
+
+                    var newkeys = [];
                     for (var i = 0; i < keys.length; ++ i) {
                         var widget = o2[keys[i]];
                         if (! context_widgets.check_condition(tab,widget)) 
                             continue;
+                        newkeys.push(keys[i]);
+                    }
+                    keys = newkeys;
+                    keys.sort(function (a, b) {
+                        var ord_a = o2[a].ord || 0.0;
+                        var ord_b = o2[b].ord || 0.0;
+                        if (ord_a < ord_b) return -1;
+                        if (ord_a > ord_b) return 1;
+                        return 0;
+                    });
+
+                    for (var i = 0; i < keys.length; ++ i) {
+                        var widget = o2[keys[i]];
+                        var ord = widget.ord || 0.0;
                         html += '<li class="ui-state-default">';
+                        html += '<div class="widget_body" data-widget-id="'+widget.uuid+'" data-ord="' + ord + '">'
                         html += context_widgets.render_html(widget);
+                        html += '</div>'
                         html += '<a href="" class="widget_delete_button" data-widget-id="' + widget.uuid + '"></a>';
                         html += "</li>";
                     }
@@ -160,11 +264,48 @@ var context_widgets = {
                             });
                         });
                     
-                    $("#widget_list").sortable();
+                    $("#widget_list").sortable({
+                        update: function(event, ui) {
+                            var before = ui.item.before();
+                            var next = ui.item.next();
+                            var hasBefore = ui.item != before;
+                            var hasNext = ui.item != next;
+                            var beforeOrd = 0.0;
+                            var nextOrd = 0.0;
+                            if (hasBefore) {
+                                beforeOrd = parseFloat(
+                                    before.children(".widget_body").data("ord"));
+                            }
+                            if (hasNext) {
+                                nextOrd = parseFloat(
+                                    next.children(".widget_body").data("ord"));
+                            }
+                            var uuid = 
+                                ui.item.children(".widget_body").data("widgetId");
+                            var newOrd = parseFloat(
+                                ui.item.children(".widget_body").data("ord"));
+
+                            if (hasBefore && hasNext) {
+                                newOrd = (beforeOrd + nextOrd) / 2.0;
+                            } else if (hasBefore) {
+                                newOrd = beforeOrd + delta_order;
+                            } else if (hasNext) {
+                                newOrd = nextOrd - delta_order;
+                            }
+                            ui.item.children(".widget_body").data("ord", newOrd);
+                            context_widgets.update_order(chrome.storage.sync,
+                                                         uuid, newOrd, 
+                                                         function () { });
+                        }
+                    });
                     $("#widget_list").disableSelection();
                     $("#add_link").button().click(function (ev) {
                         ev.preventDefault();
                         context_widgets.add_link();
+                    });
+                    $("#add_recent_page").button().click(function (ev) {
+                        ev.preventDefault();
+                        context_widgets.add_recent_pages();
                     });
                 });
             });
